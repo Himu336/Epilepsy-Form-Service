743a75e8cc8dd9aeff1ddb2b0f870988f03728ca
Himansh <himanshbansal200521@gmail.com>
2025-06-15 07:54:08 +0530

    Updated the Post route for Checklists

--- src/routes/v1/checklist-routes.js
+++ src/routes/v1/checklist-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ChecklistController = require('../../controllers/checklist-controller');
 
-router.post('/', ChecklistController.createChecklist);
+router.post('/create', ChecklistController.createChecklist);
 router.get('/:id', ChecklistController.getChecklist);
 router.get('/patient/:patient_id', ChecklistController.getChecklistsByPatient);
 
